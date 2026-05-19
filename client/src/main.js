b335cc3a0327b8d4d26eba0c5db42e396a76f049
Javier Cómbita Tellez <javiercombita2014@gmail.com>
2026-05-19 15:31:40 -0500

    feat: actualizar productos en la base de datos y agregar alertas de éxito

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,61 @@
 import './styles/globals.css';
+import { alertaExitosa } from './utils/alerts';
 
-const endopoint = "http://localhost:3000/productos"
+const endpoint = "http://localhost:3000/productos"
 
-async function traeDatos(){
+const formulario = document.getElementById("product-form")
+const nombreProducto = document.getElementById("nombre")
+const precioUnidad = document.getElementById("precio")
+const stock = document.getElementById("stock")
+const descripcionProducto = document.getElementById("descripcion")
 
-    const response = await fetch(endopoint)
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const productoNuevo = {
+        nombre: nombreProducto.value.toLowerCase().trim(),
+        precioUnidad: Number(precioUnidad.value),
+        stock: Number(stock.value),
+        descripcion: descripcionProducto.value.toLowerCase().trim()
+    }
+
+    agregarProducto(productoNuevo)
+
+
+
+
+    event.target.reset()
+})
+
+
+
+
+
+
+async function traeDatos() {
+
+    const response = await fetch(endpoint)
     const productos = await response.json()
     pintarLosDatos(productos)
+}
 
+
+async function agregarProducto(producto) {
+
+    const response = await fetch(endpoint,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify(producto)
+        }
+    )
+
+    if (response.ok) {
+        traeDatos()
+        alertaExitosa("Producto agregado exitosamente")
+    }
 }
 
 traeDatos()
@@ -32,12 +80,12 @@ function pintarLosDatos(productos) {
             <td class="px-8 py-6 text-center font-bold text-slate-900">COP ${producto.precioUnidad}</td>
             <td class="px-8 py-6 text-right">
                 <div class="flex justify-end gap-3">
-                <button class="w-10 h-10 flex items-center justify-center text-indigo-600 hover:bg-indigo-50 rounded-xl transition-all border border-transparent hover:border-indigo-100" title="Editar">
+                <button  data-id="${producto.id}" class="w-10 h-10 flex items-center justify-center text-indigo-600 hover:bg-indigo-50 rounded-xl transition-all border border-transparent hover:border-indigo-100" title="Editar">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
                 </button>
-                <button class="w-10 h-10 flex items-center justify-center text-rose-600 hover:bg-rose-50 rounded-xl transition-all border border-transparent hover:border-rose-100" title="Eliminar">
+                <button data-id="${producto.id}"  class="w-10 h-10 flex items-center justify-center text-rose-600 hover:bg-rose-50 rounded-xl transition-all border border-transparent hover:border-rose-100" title="Eliminar">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
@@ -48,5 +96,8 @@ function pintarLosDatos(productos) {
         `
     }
 
-    
+
 }
+
+
+
